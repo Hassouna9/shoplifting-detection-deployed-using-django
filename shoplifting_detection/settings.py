716a470405f6acc9f5af8b7e6665f1e6aba0b78b
Ahmed Hassouna <ahmedashraf.aa02@gmail.com>
2024-10-11 21:16:23 +0300

    Remove the secret key

--- shoplifting_detection/settings.py
+++ shoplifting_detection/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hv2o2fb=)krfm2inun9v&lbkqt6rlfcly#4i*$(er(@hm)%l1y'
+SECRET_KEY = 'YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
